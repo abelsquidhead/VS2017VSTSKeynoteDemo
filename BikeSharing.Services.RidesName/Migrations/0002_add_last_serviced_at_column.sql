9a35c2741abf1a0683febaf8e81cd6aa0a1bad9d
Abel Wang <abewan@microsoft.com>
2017-02-28 11:43:55 -0800

    added new column to DB

---
+++ BikeSharing.Services.RidesName/Migrations/0002_add_last_serviced_at_column.sql
@@ -0,0 +1,10 @@
+﻿-- <Migration ID="3b8750f5-8881-47ee-8b57-5a86696fdb7a" />
+GO
+
+PRINT N'Altering [dbo].[bikes]'
+GO
+ALTER TABLE [dbo].[bikes] ADD
+[LastServicedAt] [datetime2] NULL
+GO
+UPDATE dbo.bikes SET	LastServicedAt = InCirculationSince
+GO
