fe1136fcbf46f32276cbd711b912c26c884bba77
Abel <abewan@microsoft.com>
2017-03-01 06:51:37 +0000

    added another unit test

---
+++ AutomatedUITests/UnitTest2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AutomatedUITests
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
