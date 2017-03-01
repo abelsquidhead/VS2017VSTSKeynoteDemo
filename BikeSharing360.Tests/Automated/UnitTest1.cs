62b11dcc666a83b79636ea9372e044214f1840df
Abel <abewan@microsoft.com>
2017-03-01 07:21:13 +0000

    fixing tests

---
+++ BikeSharing360.Tests/Automated/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BikeSharing360.Tests.Automated
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [TestCategory("UI Test")]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
